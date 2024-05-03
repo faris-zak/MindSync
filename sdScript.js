7d63f83ff54388f93d4bd15910e4f4f1dafdc054
Al-Faris Mujahid AlZakwani <107947850+faris-zak@users.noreply.github.com>
2024-05-03 11:48:30 +0400

    Add files via upload

--- sdScript.js
+++ sdScript.js
@@ -1,19 +1,13 @@
-function myFunction() {
-    document.getElementById("myDownList").classList.toggle("show");
-}
-        
-function filterFunction() {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  div = document.getElementById("myDownList");
-  a = div.getElementsByTagName("a");
-  for (i = 0; i < a.length; i++) {
-    txtValue = a[i].textContent || a[i].innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      a[i].style.display = "";
-    } else {
-      a[i].style.display = "none";
+function search_animal(){
+  let input = document.getElementById('searchbar').value
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName('sections');
+  
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+    x[i].style.display = "none";
     }
-  }
-}
+    else {
+    x[i].style.display = "list-item";
+    }
+}};
