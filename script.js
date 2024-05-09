293b1739d49b84367732c3aa82f66733a6c4ba22
Al-Faris Mujahid AlZakwani <107947850+faris-zak@users.noreply.github.com>
2024-05-10 00:57:19 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-window.addEventListener('load', () => {
-  registerSW();
-});
-// Register the Service Worker
-async function registerSW() {
-  if ('serviceWorker' in navigator) {
-    try {
-      await navigator
-            .serviceWorker
-            .register('serviceworker.js');
-    }
-    catch (e) {
-      console.log('SW registration failed');
-    }
-  }
-}
-
-
 console.log("Connected Successfully.");
 
 
