24412adf38bb14b128300b03fbceec53fc26b422
Al-Faris Mujahid AlZakwani <107947850+faris-zak@users.noreply.github.com>
2024-05-15 00:02:09 +0400

    Add files via upload

--- aboScript.js
+++ aboScript.js
@@ -3,24 +3,30 @@ const userGuideSectionTitle = document.getElementById('userGuideSectionTitle');
 const userGuideSectionBody = document.getElementById('userGuideSectionBody');
 const iconSectionList = document.getElementById('iconSectionList');
 const userGuideLine = document.getElementById('userGuideLine');
+const downIcon = document.getElementById('downIcon');
 
 function showhideDiv(){
-    if(userGuideSectionTitle.style.marginBottom === '0px' &&
+    if(userGuideSectionBody.style.opacity === '0' &&
+       userGuideSectionBody.style.display === 'none' &&
        userGuideLine.style.opacity === '0' &&
        userGuideLine.style.display === 'none' &&
        iconSectionList.style.opacity === '0' &&
        iconSectionList.style.display === 'none'){
 
-        userGuideSectionTitle.style.marginBottom = '20px';
+        downIcon.style.opacity = '0';
         userGuideLine.style.opacity = '1';
         userGuideLine.style.display = 'block';
         iconSectionList.style.opacity = '1';
         iconSectionList.style.display = 'block';
+        userGuideSectionBody.style.opacity = '1';
+        userGuideSectionBody.style.display = 'block';
     } else{
-        userGuideSectionTitle.style.marginBottom = '0px';
+        downIcon.style.opacity = '1';
         userGuideLine.style.opacity = '0';
         userGuideLine.style.display = 'none';
         iconSectionList.style.opacity = '0';
         iconSectionList.style.display = 'none';
+        userGuideSectionBody.style.opacity = '0';
+        userGuideSectionBody.style.display = 'none';
     }
 };
