fb1137509a6b97d88a6ae481daf893c8aba57e92
Al-Faris Mujahid AlZakwani <a.alzakwani@outlook.com>
2024-07-15 13:09:53 +0400

    Back to MindSync

--- script.js
+++ script.js
@@ -37,16 +37,3 @@ const siteId = 5039200;
 const hotjarVersion = 6;
 
 Hotjar.init(siteId, hotjarVersion);
-
-
-
-  Sentry.onLoad(function() {
-    Sentry.init({
-      integrations: [
-        Sentry.replayIntegration(),
-      ],
-      // Session Replay
-      replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-      replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-    });
-  });
