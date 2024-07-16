2b3327adc37cd295d506770f39db1f1d81dd78d5
Al-Faris Mujahid AlZakwani <a.alzakwani@outlook.com>
2024-07-16 13:56:15 +0400

    "Updated CSS styles for header navigation, added media queries for responsive design, and modified HTML structure for header navigation. Added JavaScript code to dynamically change logo image source based on screen width."

---
+++ platform-25/assets/js/script.js
@@ -0,0 +1,15 @@
+const mindsyncBrand = document.getElementById('mindsyncBrand');
+
+function applyStyles(x) {
+
+    if (x.matches) { 
+        mindsyncBrand.getAttribute('src');
+        mindsyncBrand.setAttribute('src', 'assets/images/mindsynclogo.png');
+    } else {
+        mindsyncBrand.setAttribute('src', 'assets/images/mindsync-logo.png');
+    }
+  }
+  
+  const x = window.matchMedia("(max-width: 540px)");
+  applyStyles(x);
+  x.addListener(applyStyles);
