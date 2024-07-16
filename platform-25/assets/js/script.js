30c32560daae213d38e3ab6c2153426c5d038419
Al-Faris Mujahid AlZakwani <a.alzakwani@outlook.com>
2024-07-16 15:39:11 +0400

    "Updated CSS styles, added JavaScript functions for dropdown menu and logo tuning, and modified HTML structure for sidebar and dropdown menu."

--- platform-25/assets/js/script.js
+++ platform-25/assets/js/script.js
@@ -1,5 +1,7 @@
 const mindsyncBrand = document.getElementById('mindsyncBrand');
 
+
+                // MindSync Logo Tuning
 function applyStyles(x) {
   if (x.matches) { 
       mindsyncBrand.getAttribute('src');
@@ -13,7 +15,38 @@ const x = window.matchMedia("(max-width: 540px)");
 applyStyles(x);
 x.addListener(applyStyles);
 
-
 mindsyncBrand.addEventListener("contextmenu", (event) => {
     event.preventDefault();
 });
+
+
+
+                // Sidebar Buttons
+
+const stButton = document.getElementById('stButton');
+const ndButton = document.getElementById('ndButton');
+const rdButton = document.getElementById('rdButton');
+const thButton = document.getElementById('thButton');
+
+
+
+                // Dropdown Menu
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  function filterFunction() {
+    const input = document.getElementById("myInput");
+    const filter = input.value.toUpperCase();
+    const div = document.getElementById("myDropdown");
+    const a = div.getElementsByTagName("a");
+    for (let i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  }
