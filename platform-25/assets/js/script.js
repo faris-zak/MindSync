85468e222c197c5e672761f58108f52938de7e68
Al-Faris Mujahid AlZakwani <a.alzakwani@outlook.com>
2024-07-16 14:46:25 +0400

    "Refactored applyStyles function and moved media query and event listener declarations outside of function"

--- platform-25/assets/js/script.js
+++ platform-25/assets/js/script.js
@@ -1,19 +1,17 @@
 const mindsyncBrand = document.getElementById('mindsyncBrand');
 
 function applyStyles(x) {
-
-    if (x.matches) { 
-        mindsyncBrand.getAttribute('src');
-        mindsyncBrand.setAttribute('src', 'assets/images/mindsynclogo.png');
-    } else {
-        mindsyncBrand.setAttribute('src', 'assets/images/mindsync-logo.png');
-    }
+  if (x.matches) { 
+      mindsyncBrand.getAttribute('src');
+      mindsyncBrand.setAttribute('src', 'assets/images/mindsynclogo.png');
+  } else {
+      mindsyncBrand.setAttribute('src', 'assets/images/mindsync-logo.png');
   }
-  
-  const x = window.matchMedia("(max-width: 540px)");
-  applyStyles(x);
-  x.addListener(applyStyles);
+}
 
+const x = window.matchMedia("(max-width: 540px)");
+applyStyles(x);
+x.addListener(applyStyles);
 
 
 mindsyncBrand.addEventListener("contextmenu", (event) => {
