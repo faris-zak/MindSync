76a94c3471b000bd332e2b93771deca4db9e85bd
Al-Faris Mujahid AlZakwani <a.alzakwani@outlook.com>
2024-07-17 21:42:10 +0400

    "Updated home.js to toggle display of home and membership/membership elements, and added updateButton function. Modified index.html to call updateButton on link click and update href attribute."

--- platform-25/assets/js/home.js
+++ platform-25/assets/js/home.js
@@ -1,10 +1,10 @@
 function joinNow() {
     window.location.href = '#membership';
-    // home.style.display = 'none';
-    // membership.style.display = 'block';
+    home.style.display = 'none';
+    membership.style.display = 'block';
 }
 
-// function joinLater() {
-//     home.style.display = 'none';
-//     membership.style.display = 'block';
-// }
+function updateButton() {
+    home.style.display = 'none';
+    chapter1.style.display = 'block';
+}
