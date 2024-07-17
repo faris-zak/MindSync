558ac56003c31574908abf7c83817533dbf43182
Al-Faris Mujahid AlZakwani <a.alzakwani@outlook.com>
2024-07-17 20:13:52 +0400

    "Updated CSS, JS, and HTML files in platform-25: modified styles, added animations, and changed layout and content in home page and footer section."

--- platform-25/assets/js/home.js
+++ platform-25/assets/js/home.js
@@ -8,3 +8,20 @@ function joinLater() {
     home.style.display = 'none';
     membership.style.display = 'block';
 }
+
+const contentAnimation = document.getElementById('myDropdown');
+let clickCount = 0;
+
+contentAnimation.addEventListener('click', function() {
+    clickCount++;
+    
+    if (clickCount % 2 === 1) {
+        // First click, fade in
+        this.classList.remove('fadeOutList');
+        this.classList.add('fadeInList');
+    } else {
+        // Second click, fade out
+        this.classList.remove('fadeInList');
+        this.classList.add('fadeOutList');
+    }
+});
