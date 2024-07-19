955a7fd4efea01f1d183651d04a1d9558abb96f7
Al-Faris Mujahid AlZakwani <a.alzakwani@outlook.com>
2024-07-19 11:00:49 +0400

    "Added file card component with modal functionality, updated CSS styles, and modified HTML structure in multiple files."

--- platform-25/assets/js/chapter1.js
+++ platform-25/assets/js/chapter1.js
@@ -14,3 +14,61 @@ document.getElementById('search-bar').addEventListener('keyup', function() {
         }
     }
 });
+
+
+
+                        // File Card
+document.addEventListener('DOMContentLoaded', () => {
+    // Set file names in span tags based on data-file-path
+    document.querySelectorAll('.view-file').forEach(button => {
+        var filePath = button.getAttribute('data-file-path');
+        var fileName = filePath.split('/').pop();
+        button.nextElementSibling.innerText = fileName;
+    });
+
+    // Add event listeners to buttons
+    document.querySelectorAll('.view-file').forEach(button => {
+        button.addEventListener('click', function() {
+            var filePath = this.getAttribute('data-file-path');
+            var modal = document.getElementById('fileModal');
+            var iframe = document.getElementById('fileFrame');
+            var modalFileName = document.getElementById('modalFileName');
+            var downloadButton = document.getElementById('downloadFileButton');
+            
+            // Extract the file name from the file path
+            var fileName = filePath.split('/').pop();
+            
+            // Set the file path and name to the iframe source and modal file name
+            iframe.src = filePath;
+            modalFileName.innerText = fileName;
+            
+            // Set the file path to the download button href
+            downloadButton.href = filePath;
+            
+            // Display the modal
+            modal.style.display = 'block';
+        });
+    });
+
+    // Get the close button elements
+    var span = document.getElementById('closeButton');
+    var closeModalButton = document.getElementById('closeModalButton');
+
+    // Function to close the modal
+    function closeModal() {
+        var modal = document.getElementById('fileModal');
+        modal.style.display = 'none';
+    }
+
+    // When the user clicks on span (x) or the close button, close the modal
+    span.onclick = closeModal;
+    closeModalButton.onclick = closeModal;
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        var modal = document.getElementById('fileModal');
+        if (event.target == modal) {
+            closeModal();
+        }
+    };
+});
