bfb3cfd249e24ea720c812303c5956b51d32ff2e
Al-Faris Mujahid AlZakwani <a.alzakwani@outlook.com>
2024-07-27 05:01:54 +0400

    "Added service worker registration and manifest link to index.html"

--- platform-25/assets/js/script.js
+++ platform-25/assets/js/script.js
@@ -1,3 +1,16 @@
+                // Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, (error) => {
+      console.log('ServiceWorker registration failed: ', error);
+    });
+  });
+}
+
+
+
                 // Clear Fragment
 document.getElementById('clearFragment').addEventListener('click', function() {
     // Clear fragment identifier from URL
